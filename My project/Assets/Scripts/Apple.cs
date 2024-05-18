565ae8d87949ce82fd696c0207a044bafb352f6e
aeriel999 <ok4743@gmail.com>
2024-05-18 11:51:21 +0300

    Complete game

--- My project/Assets/Scripts/Apple.cs
+++ My project/Assets/Scripts/Apple.cs
@@ -16,7 +16,13 @@ public class Apple : MonoBehaviour
     void Update()
     {
 		if (transform.position.y < bottomY)
+		{
 			Destroy(this.gameObject);
+
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+
+			apScript.AppleDestroyed();
+		}
 
 	}
 }
--- My project/Assets/Scripts/Basket.cs
+++ My project/Assets/Scripts/Basket.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+	[Header("Set Dynamically")]
+	public Text scoreGT;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		GameObject scoreGO = GameObject.Find("ScoreCounter");
+		scoreGT = scoreGO.GetComponent<Text>();
+		scoreGT.text = "0";  
 	}
 
 	// Update is called once per frame
@@ -31,6 +37,16 @@ public class Basket : MonoBehaviour
 		if (collidedWith.CompareTag("Apple"))
 		{
 			Destroy(collidedWith);
+
+			int score = int.Parse(scoreGT.text);
+			score += 100;
+			scoreGT.text = score.ToString();
+
+			if (score > HighScore.score)
+			{
+				HighScore.score = score;
+			}
+
 		}
 	}
 }
